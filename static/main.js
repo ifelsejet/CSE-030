8ea75866abf4fb3a2a54f4a9eff59c625b60497e
Jatnael Montes <ifelsejet@gmail.com>
2020-04-28 22:53:52 -0700

    Working version of images

--- static/main.js
+++ static/main.js
@@ -5,15 +5,23 @@ $(document).ready(function(){
 		var mainContainer = document.getElementById("area");
 		mainContainer.style.justifyContent = "flex-start";
 		mainContainer.style.maxHeight = "200px";
+		var htmlCode = "";
 		for(var i in vals){
-			for(var j in vals[i]){
-				var div = document.createElement("div");
-				div.innerHTML = "<img src = images/" + vals[i][j] + ">";
+			//for(var j in vals[i]){
+				//var div = document.createElement("div");
+				//div.innerHTML = "<img src = images/" + vals[i][j] + ">";
 				//div.style.maxHeight = "200px";
 				//div.style.justifyContent = "flex-start";
-				mainContainer.appendChild(div);
-			}
+				//mainContainer.appendChild(div);
+				var currentList = vals[i];
+				for(var j in currentList){
+				var filename = currentList[j];
+				htmlCode += '<img style="width:200px;"src="images/' + filename + '">'
+				}
+			//}
 		}
+
+		$("#area").html(htmlCode);
 	});
 
 	// The Search button should be programmed to hit the /search endpoint along with the query string from the text box
@@ -21,12 +29,12 @@ $(document).ready(function(){
 		var query = $("#search-input").val();
 		$.get("/search", {"query" : query}, function(response){
 			var resp = JSON.parse(response);
-			var mainContainer = document.getElementById("area");
-			mainContainer.innerHTML = "";
+			var area = document.getElementById("area");
+			area.innerHTML = "";
 			for (var i in resp) {
 				var div = document.createElement("div");
-				div.innerHTML = "<img src = images/" + resp[i] + ">";
-				mainContainer.appendChild(div);
+				div.innerHTML = '<img style="width:200px;"src = "images/' + resp[i] + '">';
+				area.appendChild(div);
 			}
 		})
 	})
